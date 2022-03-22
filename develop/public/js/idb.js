59b102a1ca0dd64836f87020b3d8c46277cc3b84
KJ Jackson <kjjackson619@yahoo.com>
2022-03-21 20:21:04 -0400

    clear storage

--- develop/public/js/idb.js
+++ develop/public/js/idb.js
@@ -25,4 +25,45 @@ function saveRecord(record) {
     const budgetObjectStore = transaction.objectStore('new_budget');
 
     budgetObjectStore.add(record);
-}
+};
+
+function uploadBudget() {
+    const transaction = db.transaction(['new_budget'], 'readwrite');
+
+    const budgetObjectStore = transaction.objectStore('new_budget');
+
+    const getAll = budgetObjectStore.getAll();
+
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+            fetch('/api/budget', {
+                method: 'POST',
+                body: JSON.stringify(getAll.results),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then(serverResponse => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
+
+                    const transaction = db.transaction(['new_budget'], 'readwrite');
+
+                    const budgetObjectStore = transaction.objectStore('new_budget');
+
+                    budgetObjectStore.clear();
+
+                    alert('All saved transactions have been submitted!');
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+    };
+
+};
+
+window.addEventListener('online', uploadBudget);
